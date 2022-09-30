44cd699020cdacad527e9cb2612c1feaad692eca
nasibazam <nasibazam@gmail.com>
2022-10-01 01:54:47 +1000

    Second change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import Header from './components/Header'
+import { useState } from 'react';
+
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Front Entrance Broken',
+        day: 'Feb 5th',
+        reminder: true,
+    },
+    
+    {
+        id: 2,
+        text: 'Require more surveillance camera',
+        day: 'Feb 6th',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Water dropping from ceiling',
+        day: 'Feb 7th',
+        reminder: true,
+    }
+ ] 
+)
+
+// Add Tasks
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*10000)+ 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+// Delete
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id));
+  }
+// Checking
+const handle = () => {
+  console.log('hello w');
+}
+// Taggle
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => 
+  task.id === id ? {...task, reminder:
+     !task.reminder} : task));
+}
+
   return (
     <div className= 'container'>
-   <h1>Damage Reporting</h1>
-   <Header title= 'Form'/>
+      <h1>Property Damage Reporting</h1>
+      <Header title= 'Form'/>
+      <button onClick={handle}>click</button >
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : (
+        'No task to show')}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
 import PropTypes from 'prop-types'
-import React from 'react';
+import Button from './Button';
 
-const Header = (props) => {
+const Header = ({title}) => {
+  const onClick = () => {
+    console.log('Click')
+  }
   return (
-    <header>
-      <h1>{props.title}</h1>
+    <header className = 'header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick= {onClick}/>
     </header>
   );
 }
-Header.prototypes={
-  title:PropTypes.string.isRequired,
+
+Header.defaultProps = {
+  title: 'Property Damage Report Form',
 }
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
 export default Header;
+
